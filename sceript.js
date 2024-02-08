42fbbee75903d3e053e968981ea13de894b6e7a9
Qarshiev Suyun <qarshiyevsuyun28@gmail.com>
2024-02-08 09:57:47 +0500

    Initial commit

---
+++ sceript.js
@@ -0,0 +1,33 @@
+// const firsName = "Alebek"
+// const job = "oquvchi";
+// const birthYear = 2009;
+// const currentYear =2024;
+
+
+// console.log("mening ismim" + firsName + (currentYear + birthYear) + "yoshli" ${job});
+
+// console.log("hello /n nov")
+
+// const age = 20;
+
+// if (age < 19) {
+//     console.log("Siz holi parava  yoshida emasz")
+// }
+
+// const inpoutYear = 1990;
+// console.log(Number(inpoutYear) + 20);
+
+
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n); 10 
+
+// const money = 100;
+// if (money) {
+//     console.log("hamasini sarflama");
+
+// }
+// else {
+//     console.log("sen darxol ish topishing kerak")
+// }
+
